2df8eb1f2acbbb69501b761f207acc615fcd97b5
Jonny Bergdahl <jonny@bergdahl.it>
2021-04-05 22:26:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FreeTouchDeck-MaterialDesignIcons
+
+This repository contains [Material Design](https://github.com/Templarian/MaterialDesign) icons in bitmap 
+format suitable for use in [FreeTouchDeck](https://github.com/DustinWatts/FreeTouchDeck) (BMP/24 bits/75x75px).
+
+The icons can be found in the `icons` folder, all 5962 of them.
+
+The bitmaps are created by cloning the Material Design repository and 
+running the python script `makeicons.py`
+
+Installing Python requirements:
+
+`pip3 install -r requirements.txt`
+
+_Note:_ On macOS it may be necessary to install vips using Brew:
+
+`brew install vips`
---
+++ makeicons.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+"""makeicons.py: Creates 75x75 pixels BMP files out of MaterialDesign icons."""
+
+__author__ = "Jonny Bergdahl"
+__copyright__ = "Copyright 2021, Bergdahl IT AB"
+
+import os
+import glob
+from pathlib import Path
+import shutil
+import fnmatch
+from git import Git
+import pyvips
+from PIL import Image, ImageOps
+import numpy
+
+BUILD_DIR = 'build'
+MDI_PATH = 'MaterialDesign/svg'
+ICON_DIR = 'icons'
+MDI_REPO = 'https://github.com/Templarian/MaterialDesign.git'
+
+print("makeicons executing")
+build_dir = os.path.join(os.path.curdir, BUILD_DIR)
+icons_dir = os.path.join(os.path.curdir, ICON_DIR)
+svg_path = os.path.join(build_dir, MDI_PATH)
+
+if os.path.exists(build_dir):
+    # Clean up old work directory
+    print(f'Cleaning work directory {build_dir}')
+    shutil.rmtree(build_dir)
+
+print('Creating build directory')
+os.mkdir(build_dir)
+
+print(f'Cloning MaterialDesign repository into {build_dir}')
+repo = Git(build_dir)
+result = repo.clone(MDI_REPO)
+
+svg_files_count = len(fnmatch.filter(os.listdir(svg_path), '*.svg'))
+print(f'Found {svg_files_count} files in {svg_path}')
+
+# Create bitmaps
+for svg_file in glob.iglob(os.path.join(svg_path, '*.svg')):
+    file_name = Path(os.path.basename(svg_file)).stem
+    print(f'Processing {file_name}.svg', end = '')
+
+    # Load SVG file and save as PNG for PIL
+    svg = pyvips.Image.thumbnail(svg_file, 75, height=75)
+    output_png_name = os.path.join(build_dir, 'temp.png')
+    svg.write_to_file(output_png_name)
+    print('.', end = '')
+
+    # Use PIL top open file
+    png = Image.open(output_png_name)
+    print('.', end = '')
+
+    # Convert to numpy arrays for image manipulation
+    rgba = numpy.array(png)
+    # Set all non transparent pixels to white
+    rgba[rgba[...,-1] != 0] = [255,255,255,255]
+    print('.', end = '')
+    # Set all transparent pixels to black
+    rgba[rgba[...,-1] == 0] = [0,0,0,0]
+    print('.', end = '')
+
+    # Convert array back to Image
+    img = Image.fromarray(rgba)
+    print('.', end = '')
+
+    # Convert ot 24-bit RGB
+    bmp = img.convert(mode='RGB', colors=24)
+    print('.', end = '')
+
+    # Save as BMP
+    output_file_name = os.path.join(icons_dir, f'{file_name}.bmp')
+    print(f' => {output_file_name}')
+    bmp.save(output_file_name, 'BMP')
+
+print('Cleaning up')
+shutil.rmtree(build_dir)
+print('All done!')
