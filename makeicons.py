fa5a02548c8e4d00e046865c7aa24d84d5349685
Jonny Bergdahl <jonny@bergdahl.it>
2021-04-07 22:48:50 +0200

    Move icons to releases

--- makeicons.py
+++ makeicons.py
@@ -6,12 +6,14 @@ __author__ = "Jonny Bergdahl"
 __copyright__ = "Copyright 2021, Bergdahl IT AB"
 
 import os
+from os.path import basename
 import glob
 from pathlib import Path
 import shutil
 import fnmatch
 from git import Git
 import pyvips
+from zipfile import ZipFile
 from PIL import Image, ImageOps
 import numpy
 
@@ -29,9 +31,14 @@ if os.path.exists(build_dir):
     # Clean up old work directory
     print(f'Cleaning work directory {build_dir}')
     shutil.rmtree(build_dir)
+if os.path.exists(build_dir):
+    # Clean up old work directory
+    print(f'Cleaning icons directory {icons_dir}')
+    shutil.rmtree(icons_dir)
 
-print('Creating build directory')
+print('Creating work directories')
 os.mkdir(build_dir)
+os.mkdir(icons_dir)
 
 print(f'Cloning MaterialDesign repository into {build_dir}')
 repo = Git(build_dir)
@@ -40,43 +47,50 @@ result = repo.clone(MDI_REPO)
 svg_files_count = len(fnmatch.filter(os.listdir(svg_path), '*.svg'))
 print(f'Found {svg_files_count} files in {svg_path}')
 
-# Create bitmaps
-for svg_file in glob.iglob(os.path.join(svg_path, '*.svg')):
-    file_name = Path(os.path.basename(svg_file)).stem
-    print(f'Processing {file_name}.svg', end = '')
+print('Creating zip file icons.zip')
+with ZipFile('icons.zip', 'w') as zip:
 
-    # Load SVG file and save as PNG for PIL
-    svg = pyvips.Image.thumbnail(svg_file, 75, height=75)
-    output_png_name = os.path.join(build_dir, 'temp.png')
-    svg.write_to_file(output_png_name)
-    print('.', end = '')
+    # Create bitmaps
+    for svg_file in glob.iglob(os.path.join(svg_path, '*.svg')):
+        file_name = Path(os.path.basename(svg_file)).stem
+        print(f'Processing {file_name}.svg', end = '')
 
-    # Use PIL top open file
-    png = Image.open(output_png_name)
-    print('.', end = '')
+        # Load SVG file and save as PNG for PIL
+        svg = pyvips.Image.thumbnail(svg_file, 75, height=75)
+        output_png_name = os.path.join(build_dir, 'temp.png')
+        svg.write_to_file(output_png_name)
+        print('.', end = '')
 
-    # Convert to numpy arrays for image manipulation
-    rgba = numpy.array(png)
-    # Set all non transparent pixels to white
-    rgba[rgba[...,-1] != 0] = [255,255,255,255]
-    print('.', end = '')
-    # Set all transparent pixels to black
-    rgba[rgba[...,-1] == 0] = [0,0,0,0]
-    print('.', end = '')
+        # Use PIL top open file
+        png = Image.open(output_png_name)
+        print('.', end = '')
 
-    # Convert array back to Image
-    img = Image.fromarray(rgba)
-    print('.', end = '')
+        # Convert to numpy arrays for image manipulation
+        rgba = numpy.array(png)
+        # Set all non transparent pixels to white
+        rgba[rgba[...,-1] != 0] = [255,255,255,255]
+        print('.', end = '')
+        # Set all transparent pixels to black
+        rgba[rgba[...,-1] == 0] = [0,0,0,0]
+        print('.', end = '')
 
-    # Convert ot 24-bit RGB
-    bmp = img.convert(mode='RGB', colors=24)
-    print('.', end = '')
+        # Convert array back to Image
+        img = Image.fromarray(rgba)
+        print('.', end = '')
 
-    # Save as BMP
-    output_file_name = os.path.join(icons_dir, f'{file_name}.bmp')
-    print(f' => {output_file_name}')
-    bmp.save(output_file_name, 'BMP')
+        # Convert ot 24-bit RGB
+        bmp = img.convert(mode='RGB', colors=24)
+        print('.', end = '')
+
+        # Save as BMP
+        output_file_name = os.path.join(icons_dir, f'{file_name}.bmp')
+        print(f' => {output_file_name}')
+        bmp.save(output_file_name, 'BMP')
+
+        # Add to zip
+        zip.write(output_file_name, basename(output_file_name))
 
 print('Cleaning up')
 shutil.rmtree(build_dir)
+shutil.rmtree(icons_dir)
 print('All done!')
